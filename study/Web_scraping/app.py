325764d9d95a721a3ccfdfcbebea351b25e73a7f
Sami Elkhayri <selkhayri@gmail.com>
2020-06-23 09:14:10 -0400

    fixed HTTP404 error requesting favicon.ico

--- study/Web_scraping/app.py
+++ study/Web_scraping/app.py
@@ -5,9 +5,11 @@ Created on Mon Jun 22 09:37:35 2020
 @author: sami
 """
 
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, send_from_directory
 from flask_pymongo import PyMongo
 import scraping
+from splinter import Browser
+import os
 
 app = Flask(__name__)
 
@@ -29,6 +31,13 @@ def scrape():
    return jsonify(mars=mars_data)
    # return "Scraping Successful!"
 
+# Add this route to stop the HTTP 404 error that results when, after flask
+# ends processing, it submits a request for favicon.ico but no route exists 
+# to service that request.
+@app.route("/favicon.ico")
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 if __name__ == "__main__":
    executable_path = {'executable_path': 'chromedriver'}
